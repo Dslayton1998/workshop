e6fc8c56308a39099a288b87871a4925906605ea
Dslayton1998 <dslayton1998@gmail.com>
2024-05-16 14:02:13 -0500

    pairboarding

--- javascript-solutions/solutions.js
+++ javascript-solutions/solutions.js
@@ -208,3 +208,76 @@ var strStr = function(haystack, needle) {
     const index = haystack.indexOf(needle);
     return index;
 };
+
+
+
+
+//* 141. Linked List Cycle * \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+/*
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer.
+Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+
+ 
+
+Example 1:
+
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
+
+Example 2:
+
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+
+
+Example 3:
+
+Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list.
+ 
+
+Constraints:
+
+The number of the nodes in the list is in the range [0, 104].
+-105 <= Node.val <= 105
+pos is -1 or a valid index in the linked-list.
+
+1. 3 -> 2 -> 0 -> -4
+        ^-----------
+
+2. IF pos = -1 return false 
+3. head = 1st node, navigate until we reach an end (no .next)
+4. tail cycles back to a node farther up the list
+5. IF it does return true        
+*/
+var hasCycle = function(head) {
+    if(head == null) {
+        return false
+    }
+
+    let list = [];
+
+    while (!list.includes(head)) {
+        if(head.next == null) {
+            return false
+        }
+
+        list.push(head)
+        head = head.next
+    }
+
+    return true
+};
+
+
+
+// head = [-21,10,17,8,4,26,5,35,33,-7,-16,27,-12,6,29,-12,5,9,20,14,14,2,13,-24,21,23,-21,5]
+// pos = -1
--- scratch.js
+++ scratch.js
@@ -1,39 +1,70 @@
 /*
-Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer.
+Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
 
  
 
 Example 1:
 
-Input: haystack = "sadbutsad", needle = "sad"
-Output: 0
-Explanation: "sad" occurs at index 0 and 6.
-The first occurrence is at index 0, so we return 0.
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
+
 Example 2:
 
-Input: haystack = "leetcode", needle = "leeto"
-Output: -1
-Explanation: "leeto" did not occur in "leetcode", so we return -1.
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+
+
+Example 3:
+
+Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list.
  
 
 Constraints:
 
-1 <= haystack.length, needle.length <= 104
-haystack and needle consist of only lowercase English characters.
+The number of the nodes in the list is in the range [0, 104].
+-105 <= Node.val <= 105
+pos is -1 or a valid index in the linked-list.
 
-------------------------------------------------------------------------------------------------------------------
-1.compare 1st char of needle to chars of haystack IF there is a match move the pointer for needle AND haystack (+1)
-2. IF counter == needle.length return the starting index
-3. IF counter == haystack.length return -1
+1. 3 -> 2 -> 0 -> -4
+        ^-----------
+
+2. IF pos = -1 return false 
+3. head = 1st node, navigate until we reach an end (no .next)
+4. tail cycles back to a node farther up the list
+5. IF it does return true        
 */
-// // var strStr = function(haystack, needle) {
-// //    let hayCounter = 0
-// //    let needCounter = 0
-// //    let index = 0
-// //    while(index <= needCounter) {
-// //    }
-// //};
-var strStr = function(haystack, needle) {
-    const index = haystack.indexOf(needle);
-    return index;
+var hasCycle = function(head) {
+    if(head == null) {
+        return false
+    }
+
+    let list = [];
+
+    while (!list.includes(head)) {
+        if(head.next == null) {
+            return false
+        }
+
+        list.push(head)
+        head = head.next
+    }
+
+    return true
 };
+
+
+
+// head = [-21,10,17,8,4,26,5,35,33,-7,-16,27,-12,6,29,-12,5,9,20,14,14,2,13,-24,21,23,-21,5]
+// pos = -1
+
+
