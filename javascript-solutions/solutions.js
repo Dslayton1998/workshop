fe62a7b15ae0ae9b2b3c89b183a0f8b650f9c75a
Dslayton1998 <dslayton1998@gmail.com>
2024-05-18 21:07:40 -0500

    Pairs solution

--- javascript-solutions/solutions.js
+++ javascript-solutions/solutions.js
@@ -340,3 +340,64 @@ var longestCommonPrefix = function(strs) {
     }
     return(word);
 };
+
+
+
+
+//* 21. Merge Two Sorted Lists * \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+/*
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+ 
+
+Example 1:
+
+
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+Example 2:
+
+Input: list1 = [], list2 = []
+Output: []
+Example 3:
+
+Input: list1 = [], list2 = [0]
+Output: [0]
+ 
+
+Constraints:
+
+The number of nodes in both lists is in the range [0, 50].
+-100 <= Node.val <= 100
+Both list1 and list2 are sorted in non-decreasing order.
+*/
+var mergeTwoLists = function(list1, list2) {
+    let ans = new ListNode();
+    let ptr = ans;
+    while(list1 && list2){
+        if(list1.val <= list2.val){
+            ans.next = new ListNode(list1.val);
+            list1 = list1.next;
+        }
+        else{
+            ans.next = new ListNode(list2.val);
+            list2 = list2.next;
+        }
+        ans = ans.next;
+    }
+    while(list1){
+        ans.next = new ListNode(list1.val);
+        list1 = list1.next;
+        ans = ans.next;
+    }
+    while(list2){
+        ans.next = new ListNode(list2.val);
+        list2 = list2.next;
+        ans = ans.next;
+    }
+    return ptr.next;
+};
