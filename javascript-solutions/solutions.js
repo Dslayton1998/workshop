6cc29e7f1d519b19a6e225629c0e4c9500e1a1b4
Dslayton1998 <dslayton1998@gmail.com>
2024-05-23 17:00:32 -0500

    DS&A

--- javascript-solutions/solutions.js
+++ javascript-solutions/solutions.js
@@ -401,3 +401,121 @@ var mergeTwoLists = function(list1, list2) {
     }
     return ptr.next;
 };
+
+
+
+//* 169. Majority Element * \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+/*
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 5 * 104
+-109 <= nums[i] <= 109
+ 
+
+Follow-up: Could you solve the problem in linear time and in O(1) space?
+        * look up *boyer-moore* alg
+        *  ^ there is a majority element (important for O(1))
+*/
+var majorityElement = function(nums) {
+    
+};
+
+
+/* Javascript Program for Bad Character Heuristic of Boyer
+Moore String Matching Algorithm */
+let NO_OF_CHARS = 256;
+ 
+// A utility function to get maximum of two integers
+function max ( a, b )
+{
+    return ( a > b ) ? a: b;
+}
+ 
+// The preprocessing function for Boyer Moore's
+// bad character heuristic
+function badCharHeuristic(str, size, badChar)
+{
+    // Initialize all occurrences as -1
+      for (let i = 0; i < NO_OF_CHARS; i++)
+           badChar[i] = -1;
+  
+      // Fill the actual value of last occurrence
+      // of a character (indices of table are ascii and values are index of occurrence)
+      for (i = 0; i < size; i++)
+           badChar[ str[i].charCodeAt(0) ] = i;
+}
+ 
+/* A pattern searching function that uses Bad
+     Character Heuristic of Boyer Moore Algorithm */
+function search(txt,pat)
+{
+    let m = pat.length;
+      let n = txt.length;
+  
+      let badChar = new Array(NO_OF_CHARS);
+  
+      /* Fill the bad character array by calling
+         the preprocessing function badCharHeuristic()
+         for given pattern */
+      badCharHeuristic(pat, m, badChar);
+  
+      let s = 0;  // s is shift of the pattern with
+                  // respect to text
+       // there are n-m+1 potential alignments
+      while(s <= (n - m))
+      {
+          let j = m - 1;
+  
+          /* Keep reducing index j of pattern while
+             characters of pattern and text are
+             matching at this shift s */
+          while(j >= 0 && pat[j] == txt[s + j])
+              j--;
+  
+          /* If the pattern is present at current
+             shift, then index j will become -1 after
+             the above loop */
+          if (j < 0)
+          {
+              document.write("Patterns occur at shift = " + s);
+  
+              /* Shift the pattern so that the next
+                 character in text aligns with the last
+                 occurrence of it in pattern.
+                 The condition s+m < n is necessary for
+                 the case when pattern occurs at the end
+                 of text */
+              //txt[s+m] is character after the pattern in text
+              s += (s + m < n)? m-badChar[txt[s + m].charCodeAt(0)] : 1;
+  
+          }
+  
+          else
+              /* Shift the pattern so that the bad character
+                 in text aligns with the last occurrence of
+                 it in pattern. The max function is used to
+                 make sure that we get a positive shift.
+                 We may get a negative shift if the last
+                 occurrence  of bad character in pattern
+                 is on the right side of the current
+                 character. */
+              s += max(1, j - badChar[txt[s + j].charCodeAt(0)]);
+      }
+}
