fecc8df0a7f9d6ec270af51f6e3a0febb3e5a95d
Dslayton1998 <dslayton1998@gmail.com>
2024-05-28 13:45:47 -0500

    Solved: 14. Longest Common Prefix

--- scratch.js
+++ scratch.js
@@ -1,60 +1,50 @@
 /*
-Given a string s consisting of words and spaces, return the length of the last word in the string.
+Write a function to find the longest common prefix string amongst an array of strings.
 
-A word is a maximal 
-substring
- consisting of non-space characters only.
+If there is no common prefix, return an empty string "".
 
  
 
 * Example 1:
 
-Input: s = "Hello World"
-Output: 5
-Explanation: The last word is "World" with length 5.
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
 
 
 * Example 2:
 
-Input: s = "   fly me   to   the moon  "
-Output: 4
-Explanation: The last word is "moon" with length 4.
-
-
-* Example 3:
-
-Input: s = "luffy is still joyboy"
-Output: 6
-Explanation: The last word is "joyboy" with length 6.
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
  
 
 Constraints:
 
-1 <= s.length <= 104
-s consists of only English letters and spaces ' '.
-There will be at least one word in s.
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lowercase English letters.
 
-    * 1. split the string of the spaces, now we have an array of words.
-    * 2. pop the last word out and return it.
-    *   IF the last word is == a space, continue to pop()
-* s = s.trim();
+* 1. iterate through the arr, and compare letters from left to right
+* 2. 
 */
-var lengthOfLastWord = function(s) {
-    const strArr = s.split(' ');
-    let lastWord = strArr.pop()
+var longestCommonPrefix = function(strs) {
+    let pref = strs[0];
+    let prefLen = pref.length;
 
-    const trim = s.trim()
-    console.log(trim)
-
-    while(lastWord === '') {
-        lastWord = strArr.pop()
+    for (let i = 1; i < strs.length; i++) {
+        let s = strs[i];
+        while (pref !== s.substring(0, prefLen)) {
+            prefLen--;
+            if (prefLen === 0) {
+                return "";
+            }
+            pref = pref.substring(0, prefLen);
+        }
     }
 
-    return lastWord.length
-// * const trim = s.trim() <=== trim can be useful, returns a str
+    return pref; 
 };
 
-// console.log(lengthOfLastWord("Hello World")) // 5
-console.log(lengthOfLastWord("   fly me   to   the moon  ")) // 4
-// console.log(lengthOfLastWord('luffy is still joyboy'))// 6
-// console.log(lengthOfLastWord(''))
+// console.log(longestCommonPrefix(["flower","flow","flight"])) // "fl"
+// console.log(longestCommonPrefix(["dog","racecar","car"])) // "" (There is no common prefix among the input strings.)
+// console.log(longestCommonPrefix())
