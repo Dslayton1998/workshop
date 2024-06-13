589b66339812d0d2042d0045572f021b9d962e90
Dslayton1998 <dslayton1998@gmail.com>
2024-06-13 15:52:22 -0500

    started 205. Isomorphic Strings

--- scratch.js
+++ scratch.js
@@ -1,49 +1,37 @@
 /*
-Write an algorithm to determine if a number n is happy.
+Given two strings s and t, determine if they are isomorphic.
 
-A happy number is a number defined by the following process:
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
 
-Starting with any positive integer, replace the number by the sum of the squares of its digits.
-Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
-Those numbers for which this process ends in 1 are happy.
-Return true if n is a happy number, and false if not.
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
 
  
 
 Example 1:
 
-Input: n = 19
+Input: s = "egg", t = "add"
 Output: true
-Explanation:
-1^2 + 9^2 = 82
-8^2 + 2^2 = 68
-6^2 + 8^2 = 100
-1^2 + 0^2 + 0^2 = 1
 Example 2:
 
-Input: n = 2
+Input: s = "foo", t = "bar"
 Output: false
+Example 3:
+
+Input: s = "paper", t = "title"
+Output: true
  
 
 Constraints:
 
-1 <= n <= 231 - 1
-* 1. split n, and square each digit in its sum, adding the totals to == the NEW n
-* 2. repeat this process until NEW n == 1
-* 3. If it ends in 1 return true
-    * - ELSE return false
+1 <= s.length <= 5 * 104
+t.length == s.length
+s and t consist of any valid ascii character.
 */
-var isHappy = function(n) {
-    const checked = [];
-    while (n !== 1) {
-        if (checked.includes(n)) return false;
-        checked.push(n);
-        n = n.toString().split('').map(i => i**2).reduce((sum, i) => sum + i, 0);
-    }
-    return true;
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    
 };
-
-
-// console.log(isHappy(19)) // true
-// console.log(isHappy(2)) // false
-// console.log(isHappy(7)) // true
