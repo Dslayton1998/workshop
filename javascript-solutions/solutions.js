594f82cf8726e420f196d8dcf66f98d2c6d2d1de
Dslayton1998 <dslayton1998@gmail.com>
2024-06-15 16:02:51 -0500

    started 242. Valid Anagram

--- javascript-solutions/solutions.js
+++ javascript-solutions/solutions.js
@@ -995,3 +995,69 @@ var isHappy = function(n) {
 // console.log(isHappy(19)) // true
 // console.log(isHappy(2)) // false
 // console.log(isHappy(7)) // true
+
+
+
+//* 205. Isomorphic Strings * \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+/*
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+ 
+
+Example 1:
+
+Input: s = "egg", t = "add"
+Output: true
+Example 2:
+
+Input: s = "foo", t = "bar"
+Output: false
+Example 3:
+
+Input: s = "paper", t = "title"
+Output: true
+ 
+
+Constraints:
+
+1 <= s.length <= 5 * 104
+t.length == s.length
+s and t consist of any valid ascii character.
+*/
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    const dict = new Map();
+    const hSet = new Set();
+
+    for(let i = 0; i< s.length;i++)
+    {
+        const sChar = s.charAt(i);
+        const tChar = t.charAt(i);
+        if (!dict.has(sChar))
+        {
+            if(hSet.has(tChar))
+            {
+                return false;
+            }
+            dict.set(sChar, tChar);
+            hSet.add(tChar);
+        }
+        else    //check if exists
+        {
+            if(tChar !== dict.get(sChar))
+            {
+                return false;
+            }
+        }
+    }
+
+    return true;
+};
