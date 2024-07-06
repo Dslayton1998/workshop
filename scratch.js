89f19edf048f0b0ba278492ee6c41e12964776c1
Dslayton1998 <dslayton1998@gmail.com>
2024-07-06 13:26:07 -0500

    Solved: 148. Sort List

--- scratch.js
+++ scratch.js
@@ -41,5 +41,23 @@ Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.e. 
  * @return {ListNode}
  */
 var sortList = function(head) {
-    
+    if(head==null){
+        return null;
+    }
+    let ptr=head;
+    let arr=[];
+    while(ptr){
+        arr.push(ptr.val);
+        ptr=ptr.next;
+    }
+    arr.sort((a,b)=>a-b);
+    let n = new ListNode(arr[0]);
+    head=n;
+    let temp=head;
+    for(let i=1;i<arr.length;i++){
+        let n1 = new ListNode(arr[i]);
+        temp.next=n1;
+        temp=temp.next;       
+    }
+    return head;
 };
