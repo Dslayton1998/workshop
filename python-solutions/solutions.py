9668797f29e03c1c54aac0edeafdad4e95c6c679
Dslayton1998 <dslayton1998@gmail.com>
2024-07-13 22:05:05 -0500

    Solved: 35. Search Insert Position PY

--- python-solutions/solutions.py
+++ python-solutions/solutions.py
@@ -94,3 +94,22 @@ def removeDuplicates(self, nums: List[int]) -> int:
         # increament to get to next streak
         r += 1
     return l
+
+#* 35. Search Insert Position *########################################################################################
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        
+        return left
+    
