6e7ff010bbb3c3b68ef43b20863e5677840b9d43
Dslayton1998 <dslayton1998@gmail.com>
2024-07-29 17:25:36 -0500

    Solved: 9. Palindrome Number

--- scratch.js
+++ scratch.js
@@ -27,5 +27,14 @@ Constraints:
 -231 <= x <= 231 - 1
 */
 var isPalindrome = function(x) {
-    
+    if (x < 0) {
+        return false;
+    }
+    let reverse = 0;
+    let xCopy = x;
+    while (x > 0) {
+        reverse = (reverse * 10) + (x % 10);
+        x = Math.floor(x / 10);
+    }
+    return reverse === xCopy;
 };
