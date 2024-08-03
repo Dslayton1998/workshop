d0bdb723443aa37f1d68bd7c1c16cf2c789dc211
Dslayton1998 <dslayton1998@gmail.com>
2024-08-02 19:46:01 -0500

    Started: 20. Valid Parentheses

--- scratch.js
+++ scratch.js
@@ -1,51 +1,32 @@
 /*
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+An input string is valid if:
 
-You can return the answer in any order.
-
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
  
 
 Example 1:
 
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
-Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Input: s = "()"
+Output: true
 Example 2:
 
-Input: nums = [3,2,4], target = 6
-Output: [1,2]
+Input: s = "()[]{}"
+Output: true
 Example 3:
 
-Input: nums = [3,3], target = 6
-Output: [0,1]
+Input: s = "(]"
+Output: false
  
 
 Constraints:
 
-2 <= nums.length <= 104
--109 <= nums[i] <= 109
--109 <= target <= 109
-Only one valid answer exists.
- 
-
-Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+1 <= s.length <= 104
+s consists of parentheses only '()[]{}'.
 */
-var twoSum = function(nums, target) {
-    //select the first number and add that to the rest of the values in the array
-    // IF that number == the target return those index's
-    // ELSE grab the next number out of the array and continue the process 
-    let solution = [];
-    let index = 0;
-    while (index < nums.length) {
-    const currentIndex = nums[index]
-        if(currentIndex + nums[index + 1] == target) {
-            solution.push(index)
-            solution.push(index + 1)
-            return solution
-        } else {
-            index++
-        }
-    }
+var isValid = function(s) {
+    
 };
