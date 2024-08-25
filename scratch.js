9f2dbcba97de240066a8393d6f9eab84c2c8691c
Dslayton1998 <dslayton1998@gmail.com>
2024-08-24 21:58:22 -0500

    Started: 1331. Rank Transform of an Array

--- scratch.js
+++ scratch.js
@@ -1,49 +1,39 @@
-/* 
-Given the root of a binary search tree and an integer k, return true if there exist two elements in the BST such that their sum is equal to k, or false otherwise.
+/*
+Given an array of integers arr, replace each element with its rank.
 
+The rank represents how large the element is. The rank has the following rules:
 
-Input: root = [5,3,6,2,4,null,7], k = 9
-Output: true
+Rank is an integer starting from 1.
+The larger the element, the larger the rank. If two elements are equal, their rank must be the same.
+Rank should be as small as possible.
+ 
 
+Example 1:
 
-Input: root = [5,3,6,2,4,null,7], k = 28
-Output: false
+Input: arr = [40,10,20,30]
+Output: [4,1,2,3]
+Explanation: 40 is the largest element. 10 is the smallest. 20 is the second smallest. 30 is the third smallest.
+Example 2:
+
+Input: arr = [100,100,100]
+Output: [1,1,1]
+Explanation: Same elements share the same rank.
+Example 3:
+
+Input: arr = [37,12,28,9,100,56,80,5,12]
+Output: [5,3,4,2,8,6,7,1,3]
  
 
 Constraints:
 
-The number of nodes in the tree is in the range [1, 104].
--104 <= Node.val <= 104
-root is guaranteed to be a valid binary search tree.
--105 <= k <= 105
+0 <= arr.length <= 105
+-109 <= arr[i] <= 109
 */
 
 /**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
+ * @param {number[]} arr
+ * @return {number[]}
  */
-/**
- * @param {TreeNode} root
- * @param {number} k
- * @return {boolean}
- */
-var findTarget = function(root, k) {
-    let set = new Set();
-    return helper(root, k, set);
+var arrayRankTransform = function(arr) {
+    
 };
-
-
-let helper = function(root, k, set){
-    if(root==null){
-        return false;
-    }
-    if(set.has(k-root.val)){
-        return true;
-    }
-    set.add(root.val);
-    return (helper(root.left, k, set) || helper(root.right, k, set));
-}
