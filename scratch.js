0db19f9c7dfa1351a2ecab9a4292a909d7a5a317
Dslayton1998 <dslayton1998@gmail.com>
2024-08-24 22:45:33 -0500

    Solved: 1331. Rank Transform of an Array

--- scratch.js
+++ scratch.js
@@ -35,5 +35,20 @@ Constraints:
  * @return {number[]}
  */
 var arrayRankTransform = function(arr) {
-    
+    let obj = {}
+    let newArr = [...arr].sort((a,b) => a-b)
+    let rank = 1
+    for(let i = 0; i < newArr.length;i++) {
+        if(obj[newArr[i]]) {
+        } else {
+            obj[newArr[i]] = rank
+            rank++
+        }
+    }
+
+    for(let i = 0; i < arr.length;i++) {
+        arr[i] = obj[arr[i]]
+    }
+
+    return arr
 };
