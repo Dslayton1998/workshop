4665a0f127b098d99fa02401b3e12a5c2a13840c
Dslayton1998 <dslayton1998@gmail.com>
2024-10-26 21:20:15 -0500

    2784. Check if Array is Good

--- scratch.js
+++ scratch.js
@@ -38,5 +38,12 @@ Constraints:
 1 <= num[i] <= 200
 */
 var isGood = function(nums) {
-    
+    if (nums.length <= 1) return false;
+    nums.sort((a, b) => a - b);
+    if (nums[nums.length - 1] !== nums[nums.length - 2]) return false;
+    if (nums[nums.length - 1] !== nums.length - 1) return false;
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] !== i + 1) return false;
+    }
+    return true;
 };
