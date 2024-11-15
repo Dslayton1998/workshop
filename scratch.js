84c8a5fc002621140222c8b4d395b2240463ce1e
Dslayton1998 <dslayton1998@gmail.com>
2024-11-15 09:10:13 -0600

    Solved: 696. Count Binary Substrings

--- scratch.js
+++ scratch.js
@@ -27,5 +27,15 @@ Constraints:
 s[i] is either '0' or '1'.
 */
 var countBinarySubstrings = function(s) {
-    
+    let count = 0, prev = 0, curr = 1;
+    for (let i = 1; i < s.length; i++) {
+        if (s[i] !== s[i - 1]) {
+            count += Math.min(prev, curr);
+            prev = curr;
+            curr = 1;
+        } else {
+            curr++;
+        }
+    }
+    return count + Math.min(prev, curr);
 };
