4a9b26c5b7935f2ca9b0887cc147aef7365349ce
Dslayton1998 <dslayton1998@gmail.com>
2024-11-30 14:39:04 -0600

    Started: 1560. Most Visited Sector in a Circular Track

--- scratch.js
+++ scratch.js
@@ -1,48 +1,38 @@
 /*
-You are given a string s consisting only of the characters '0' and '1'. In one operation, you can change any '0' to '1' or vice versa.
+Given an integer n and an integer array rounds. We have a circular track which consists of n sectors labeled from 1 to n. A marathon will be held on this track, the marathon consists of m rounds. The ith round starts at sector rounds[i - 1] and ends at sector rounds[i]. For example, round 1 starts at sector rounds[0] and ends at sector rounds[1]
 
-The string is called alternating if no two adjacent characters are equal. For example, the string "010" is alternating, while the string "0100" is not.
+Return an array of the most visited sectors sorted in ascending order.
 
-Return the minimum number of operations needed to make s alternating.
+Notice that you circulate the track in ascending order of sector numbers in the counter-clockwise direction (See the first example).
 
  
 
 Example 1:
 
-Input: s = "0100"
-Output: 1
-Explanation: If you change the last character to '1', s will be "0101", which is alternating.
+
+Input: n = 4, rounds = [1,3,1,2]
+Output: [1,2]
+Explanation: The marathon starts at sector 1. The order of the visited sectors is as follows:
+1 --> 2 --> 3 (end of round 1) --> 4 --> 1 (end of round 2) --> 2 (end of round 3 and the marathon)
+We can see that both sectors 1 and 2 are visited twice and they are the most visited sectors. Sectors 3 and 4 are visited only once.
 Example 2:
 
-Input: s = "10"
-Output: 0
-Explanation: s is already alternating.
+Input: n = 2, rounds = [2,1,2,1,2,1,2,1,2]
+Output: [2]
 Example 3:
 
-Input: s = "1111"
-Output: 2
-Explanation: You need two operations to reach "0101" or "1010".
+Input: n = 7, rounds = [1,3,5,7]
+Output: [1,2,3,4,5,6,7]
  
 
 Constraints:
 
-1 <= s.length <= 104
-s[i] is either '0' or '1'.
+2 <= n <= 100
+1 <= m <= 100
+rounds.length == m + 1
+1 <= rounds[i] <= n
+rounds[i] != rounds[i + 1] for 0 <= i < m
 */
-var minOperations = function(s) {
-    let binary = true;
-    let zero = 0, one = 0;
-
-    for(let letter of s){
-        if(binary){
-            if(letter == '0') one++;
-            else zero++;
-        }else{
-            if(letter == '0') zero++;
-            else one++;
-        }
-        binary = !binary;
-    }
-
-    return Math.min(one, zero);
+var mostVisited = function(n, rounds) {
+    
 };
