543e76a41710293ee0348ae8a4114b7ebbea848a
Dslayton1998 <dslayton1998@gmail.com>
2024-11-30 14:35:52 -0600

    Solved: 1758. Minimum Changes To Make Alternating Binary String

--- javascript-solutions/solutions.js
+++ javascript-solutions/solutions.js
@@ -3020,3 +3020,56 @@ var numberOfChild = function(n, k) {
     }
     return n - 1 - remainingTime;
 };
+
+
+
+
+//* 1758. Minimum Changes To Make Alternating Binary String * \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+/*
+You are given a string s consisting only of the characters '0' and '1'. In one operation, you can change any '0' to '1' or vice versa.
+
+The string is called alternating if no two adjacent characters are equal. For example, the string "010" is alternating, while the string "0100" is not.
+
+Return the minimum number of operations needed to make s alternating.
+
+ 
+
+Example 1:
+
+Input: s = "0100"
+Output: 1
+Explanation: If you change the last character to '1', s will be "0101", which is alternating.
+Example 2:
+
+Input: s = "10"
+Output: 0
+Explanation: s is already alternating.
+Example 3:
+
+Input: s = "1111"
+Output: 2
+Explanation: You need two operations to reach "0101" or "1010".
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s[i] is either '0' or '1'.
+*/
+var minOperations = function(s) {
+    let binary = true;
+    let zero = 0, one = 0;
+
+    for(let letter of s){
+        if(binary){
+            if(letter == '0') one++;
+            else zero++;
+        }else{
+            if(letter == '0') zero++;
+            else one++;
+        }
+        binary = !binary;
+    }
+
+    return Math.min(one, zero);
+};
--- scratch.js
+++ scratch.js
@@ -30,5 +30,19 @@ Constraints:
 s[i] is either '0' or '1'.
 */
 var minOperations = function(s) {
-    
+    let binary = true;
+    let zero = 0, one = 0;
+
+    for(let letter of s){
+        if(binary){
+            if(letter == '0') one++;
+            else zero++;
+        }else{
+            if(letter == '0') zero++;
+            else one++;
+        }
+        binary = !binary;
+    }
+
+    return Math.min(one, zero);
 };
