dd562353494357062a4b6d7cf59113957a1225ef
Dslayton1998 <dslayton1998@gmail.com>
2024-11-30 21:17:23 -0600

    Solved: 1560. Most Visited Sector in a Circular Track

--- scratch.js
+++ scratch.js
@@ -34,5 +34,14 @@ rounds.length == m + 1
 rounds[i] != rounds[i + 1] for 0 <= i < m
 */
 var mostVisited = function(n, rounds) {
+    const start = rounds[0], end = rounds[rounds.length-1];
+    const result = [];
     
+    if(start <= end) {
+        for(let i = start; i <= end; i++) result.push(i);
+    } else {
+        for(let i = 1; i <= end; i++) result.push(i);
+        for(let i = start; i <= n; i++) result.push(i);
+    }
+    return result;
 };
