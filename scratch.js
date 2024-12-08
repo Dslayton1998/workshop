4ebeec7f925184d8ed397840e176dad9fd993737
Dslayton1998 <dslayton1998@gmail.com>
2024-12-07 20:51:05 -0600

    Solved: 2928. Distribute Candies Among Children I

--- scratch.js
+++ scratch.js
@@ -23,5 +23,14 @@ Constraints:
 1 <= limit <= 50
 */
 var distributeCandies = function(n, limit) {
-    
+    let count = 0;
+    for(let i=0; i<=limit; i++){
+        for(let j=0; j<=limit; j++){
+            for(let k=0; k<=limit; k++){
+               if(i+j+k>n) break;
+               if(i+j+k === n) count ++
+            }
+        }
+    }
+     return count
 };
