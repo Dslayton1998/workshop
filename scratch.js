7e5179fd718f80a559ff7630f00ddfaaccea74ca
Dslayton1998 <dslayton1998@gmail.com>
2025-01-16 18:58:55 -0600

    Solved: 94. Binary Tree Inorder Traversal

--- scratch.js
+++ scratch.js
@@ -44,5 +44,15 @@ The number of nodes in the tree is in the range [0, 100].
 -100 <= Node.val <= 100
 */
 var inorderTraversal = function(root) {
-    
+    const result = [];
+    helper(root, result);
+    return result;
+};
+
+function helper(root, result) {
+    if (root !== null) {
+        helper(root.left, result);
+        result.push(root.val);
+        helper(root.right, result);
+    }
 };
