df9b93004f1e89dcaac91483d38e9610d25182ff
Dslayton1998 <dslayton1998@gmail.com>
2025-01-24 22:33:11 -0600

    Solved: 1859. Sorting the Sentence PY

--- python-solutions/solutions.py
+++ python-solutions/solutions.py
@@ -235,3 +235,11 @@ class Solution:
                 ans += m[s[ i ]]
         
         return ans
+    
+
+
+#* 1859. Sorting the Sentence *########################################################################################
+class Solution:
+    def sortSentence(self, s: str) -> str:
+        sentence = sorted((word[-1], word[:-1]) for word in s.split())
+        return ' '.join(pair[1] for pair in sentence)
